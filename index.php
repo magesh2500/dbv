fb1aa1f040833b0ea5e135bbbb87cc29284bbc8c
Satham Ussain <satham@webspa.in>
2017-03-01 16:08:14 +0530

    this commit was only for testing purpose

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+// satham test comment
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config.php';
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib/functions.php';
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'DBV.php';
